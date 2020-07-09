d9ef635bbc546e37e0ef1910b2ee06e4e9638013
Witelitening <witeliteningHWD@gmail.com>
2020-07-09 17:53:40 -0500

    database changes

--- dungeonsPartyBot/Main.js
+++ dungeonsPartyBot/Main.js
@@ -1,27 +1,5 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('database', 'user', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	// SQLite only
-	storage: 'database.sqlite',
-});
-const Tags = sequelize.define('tags', {
-	name: {
-		type: Sequelize.STRING,
-		unique: true,
-	},
-	description: Sequelize.TEXT,
-	username: Sequelize.STRING,
-	usage_count: {
-		type: Sequelize.INTEGER,
-		defaultValue: 0,
-		allowNull: false,
-	},
-});
 
 const client = new Discord.Client();
 client.config = require('./config.json');
@@ -35,7 +13,8 @@ for (const file of commandFiles) {
 }
 
 client.on('ready', () => {
- 	console.log(`Logged in as ${client.user.tag}!`);
+	 console.log(`Logged in as ${client.user.tag}!`);
+	 Tags.sync();
 });
 
 client.on('message', message => {require('./events/message.js').run(client, message, message.author)});
--- dungeonsPartyBot/commands/create.js
+++ dungeonsPartyBot/commands/create.js
@@ -1,16 +1,45 @@
+const fs = require('fs');
+const users = require('../users.json');
+const parties = require('../parties.json');
+
 module.exports.run = async (client, message, args, author) => {
-    console.log('In create');
     const Discord = require('discord.js');
 
+    //create user profile if not existing
+    if(!users[author.id]) {
+        users[author.id] = {
+            name = client.users.get(author.id).tag,
+            inParty = true,
+            partyID = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1),
+        }
+        fs.writeFile('../users.json', JSON.stringify(users), (err) => {
+            if(err) console.log(err);
+        });
+    }
+
+    //send embed asking for a description
     const embed1 = new Discord.MessageEmbed()
         .setColor('LUMINOUS_VIVID_PINK')
-        .setTitle('Class requests')
-        .setDescription('Do you have any special requests?');
+        .setTitle('Add description')
+        .setDescription('What would you like your party description to be?');
     message.channel.send(embed1);
 
+    //get party description/requests
+    let desc;
     const collector = message.channel.createMessageCollector(message.channel, m => m.author.id === message.author.id, { time: 15000 });
     collector.once('collect', m => {
+        desc = m.content;
         console.log(`Collected ${m.content}`);
+    });
+
+    //create party
+    parties[users[author.id].partyID] = {
+        partyLeader = client.users.get(author.id).tag,
+        description = desc,
+        partyMembers = [],
+    }
+    fs.writeFile('../parties.json', JSON.stringify(users), (err) => {
+        if(err) console.log(err);
     });
 };
 
